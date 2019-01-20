fb719d6d30e2c9f6a8dd7936b2ab0a6ea5e93a8c
ssalhan <safron_salhan@hotmail.co.uk>
2019-01-20 15:01:05 +0000

    Initial commit

---
+++ Old files/config.php
@@ -0,0 +1,16 @@
+
+
+/* Define MySQL connection details and database table name */ 
+$SETTINGS["hostname"]='localhost';
+$SETTINGS["mysql_user"]='REPLACE';
+$SETTINGS["mysql_pass"]='REPLACE';
+$SETTINGS["mysql_database"]='REPLACE';
+$SETTINGS["data_table"]='event_calendar';
+
+/* Connect to MySQL */
+
+if (!isset($install) or $install != '1') {
+	$connection = mysql_connect($SETTINGS["hostname"], $SETTINGS["mysql_user"], $SETTINGS["mysql_pass"]) or die ('Unable to connect to MySQL server.<br ><br >Please make sure your MySQL login details are correct.');
+	$db = mysql_select_db($SETTINGS["mysql_database"], $connection) or die ('request "Unable to select database."');
+};
+?>
---
+++ first pages/login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Login forms </title>
+</head>
+<body>
+<form method="Post">
+	<table>
+		<tr> 
+			<td> User Type <td>
+			<td><select name="type">
+				<option value="-1">select user type</option>
+				<option value="Student">Student</option>
+				<option value="Teacher">Teacher</option>
+				<option value="Contacts">Contacts</option>
+			</select></td>
+		</tr>
+		<tr>
+			<td>Username</td>
+			<td><input type="text" name="username" placeholder="username"></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td><input type="text" name="password" placeholder="password"></td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td><input type="submit" name="submit" placeholder="Login"></td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
+
+<?php
+$con=mysql_connect("localhost","root","");
+if(!$con){ 
+	echo'Unable to establish connection '.mysql_error();
+}
+else{
+	$db=mysql_select_db('vyners',$con);
+if(!$db)
+{
+	echo'Database not found '.mysql_error();
+}
+if (isset($_post['Submit'])){
+$type=$_post['type'];
+$un=$_post['username'];
+$pw=$_post['password'];
+
+$query="select * from login where username='$username' and password='$password'and type='$user_type'";
+$result=mysql_query($query);
+
+while($row=mysql_fetch_array($result)){
+	if($row=['username']==$username && $row['password']==$password && $row['type']='Teacher'){
+		header('Location: teacher.html');
+	}elseif($row=['username']==$username && $row['password']==$password && $row['type']='Student'){
+		header('Location: student.html');
+	}elseif($row=['username']==$username && $row['password']==$password && $row['type']='Contacts'){
+		header('Location: Contacts.html');
+		}
+	}
+}
+?>
